5792151da895049e6e3beced17ea14e2040c4765
Sckab <giulianodeamicis2009@gmail.com>
2025-11-12 23:30:53 +0100

    create a build-run script for better automation, update the README.md

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 # FIMA
 
 </div>
+
+FIMA (File Manager) is a simple and fast file manager, written in C++.
+
+## Specs
+
+| Spec | Purpose |
+|-|-|
+| C++ | Main language |
+| CMake | Build system configuration generator |
+| Make | Build system |
+| FTXUI | TUI library |
+
+
+## Scripts
+
+There is a script directory, where there are all the scripts are located.
+
+These are the most important ones (so far):
+- `scripts/get-source-files.sh`
+  - This is the script used to get all the `.cpp` files in the `src/` directory
+
+- `scripts/build-run.sh`
+  - This is the script used to build, run or both the program
+
+> [!IMPORTANT]
+> All the scripts should be run in the `scripts/` directory, so make sure to cd into it
+
---
+++ scripts/build-run.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+./get-source-files.sh
+
+cat ./utils/ascii-art.txt
+
+dir=$(basename "$(pwd)")
+
+if [ "$dir" = "scripts" ]; then
+    [ ! -d ../build ] && mkdir ../build
+
+    cd ../build || exit 1
+fi
+
+build() {
+    cmake .. || {
+        echo "CMake configuration failed"
+        exit 1
+    }
+
+    make -j"$(nproc)" || {
+        echo "Build failed"
+        exit 1
+    }
+}
+
+run() {
+    if [ ! -f ./fima ]; then
+        echo "Binary not found! Building first..."
+        build
+    fi
+
+    ./fima
+}
+
+PS3="Please enter your choice: "
+options=("Build" "Run" "Build and Run" "Quit")
+
+select opt in "${options[@]}"; do
+    case $opt in
+    "Build")
+        build
+        break
+        ;;
+    "Run")
+        run
+        break
+        ;;
+    "Build and Run")
+        build
+        run
+        break
+        ;;
+    "Quit")
+        break
+        ;;
+    *)
+        echo "invalid option $REPLY"
+        ;;
+    esac
+done
