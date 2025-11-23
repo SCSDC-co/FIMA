f76ed93c66c14a4b2f4470039ea7f2eddb4b03e4
Sckab <giulianodeamicis2009@gmail.com>
2025-11-23 18:23:14 +0100

    udpate scripts/get-source-files

--- scripts/cmake/files.cmake
+++ scripts/cmake/files.cmake
@@ -1,9 +1,9 @@
 set(SOURCE_FILES
-src/modules/tree/main.cpp
-src/modules/ls/main.cpp
-src/helpers/get_directories_entries.cpp
-src/tui/modules/tree/tree_tui.cpp
-src/tui/modules/ls/ls_tui.cpp
-src/tui/main.cpp
-src/main.cpp
+    src/helpers/get_directories_entries.cpp
+    src/tui/modules/tree/tree_tui.cpp
+    src/tui/modules/ls/ls_tui.cpp
+    src/modules/tree/main.cpp
+    src/modules/ls/main.cpp
+    src/tui/main.cpp
+    src/main.cpp
 )
--- scripts/get-source-files.sh
+++ scripts/get-source-files.sh
@@ -6,10 +6,24 @@ touch ../scripts/cmake/files.cmake
 
 cd ..
 
-FILES=$(find src/ -type f)
+FILES=$(find src/ -type f | awk '{ print length, $0 }' | sort -nr | cut -d" " -f2-)
 
 cat <<EOF >./scripts/cmake/files.cmake
 set(SOURCE_FILES
 $FILES
 )
 EOF
+
+sed -i -e '/set(SOURCE_FILES/,/)/{
+    /set(SOURCE_FILES/!{
+        /)/! s/^/    /
+    }
+}' ./scripts/cmake/files.cmake
+
+echo ""
+echo "Output: "
+echo ""
+
+cat ./scripts/cmake/files.cmake
+
+echo ""
