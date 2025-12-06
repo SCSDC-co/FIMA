f1f39fd9acc52f677aeb444395a4aff87a17b47a
Sckab <giulianodeamicis2009@gmail.com>
2025-12-06 17:07:51 +0100

    feat: add blank lines count

--- src/commands/cloc/main.cpp
+++ src/commands/cloc/main.cpp
@@ -12,17 +12,29 @@ namespace fima {
 namespace cloc {
 
 void cout_lines(fs::path path) {
+    int number_of_lines_total{0};
+    int number_of_lines_blank{0};
     int number_of_lines{0};
 
     std::string line;
     std::ifstream file(path);
 
     while (std::getline(file, line)) {
+        if (line.empty()) {
+            ++number_of_lines_blank;
+
+            continue;
+        }
+
         ++number_of_lines;
     }
 
-    std::cout << "Number of lines of " << path.string() << " - "
-              << number_of_lines << '\n';
+    number_of_lines_total = number_of_lines_blank + number_of_lines;
+
+    std::cout << "Number of lines of " << path.string() << '\n';
+    std::cout << "Total " << number_of_lines_total << '\n';
+    std::cout << "Blank " << number_of_lines_blank << '\n';
+    std::cout << "Other " << number_of_lines << '\n';
 }
 
 } // namespace cloc
