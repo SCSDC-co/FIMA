8478448aa9e38a58201074734dc6a1d1624e7823
Sckab <giulianodeamicis2009@gmail.com>
2026-08-04 17:33:00 +0200

    chore: add biome-ignore to a line

--- docs/components/Showcase.vue
+++ docs/components/Showcase.vue
@@ -1,4 +1,5 @@
 <script setup>
+// biome-ignore lint/correctness/noUnusedImports: withBase is actually used but biome doesn't recognize it
 import { withBase } from "vitepress";
 import { onMounted, ref } from "vue";
 import mediumZoom from "medium-zoom";
